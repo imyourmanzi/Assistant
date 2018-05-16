df801c95fa0d0477d77a23e09c95ab75b542b8c7
Matt Manzi <manzi.matthewr@gmail.com>
2018-05-16 11:18:19 -0400

    Updated formatting for help output and correspoding readme content

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Hi, I'm Assistant
 
-Assistant is an AI that only knows one valuable skill (addition), but with your help can learn more!
+Assistant is an AI that only knows one valuable skill (addition).  With your help they can learn more!
 
 ## Using Assistant
 
@@ -18,13 +18,13 @@ Not much; asking Assistant for help will give you the following result:
 ```
 I am Assistant. I can help you with a variety of simple tasks.
 These are the things I know how to do:
-	 Add - Find the sum of two numbers
+	Add - Find the sum of two numbers
 		In: 2 int => Out: 1 int
-	 Learn - Used to teach Assistant new skills
-		In: 1 str => Out: 0
-	 Help - Display this dialog
-		In: 0  => Out: 0
-	 Exit
+	Learn - Used to teach Assistant new skills
+		In: 1 str => Out: None
+	Help - Display this dialog
+		In: None => Out: None
+	Exit
 ```
 
 ## Want to help?
--- skills.py
+++ skills.py
@@ -8,8 +8,7 @@ import constants as c
 
 # add finds the sum of param0 and param1
 def add(param0, param1):
-    sum = param0 + param1
-    return sum
+    return param0 + param1
 
 # learn creates a new skill named by param0
 def learn(param0):
@@ -38,11 +37,6 @@ def learn(param0):
     # ask how to do it
     rawTask = input("How can I do that based on what I already know how to do? ")
 
-
-
-
-
-
 # help displays a help dialog
 def help():
     print("I am Assistant. I can help you with a variety of simple tasks.")
@@ -51,7 +45,21 @@ def help():
     with open(c.KNOWLEDGE, "r") as knowledgeFile:
         skills = json.load(knowledgeFile)
         for s in skills:
-            print("\t", s.capitalize(), "-", skills[s][c.DESC])
-            print("\t\tIn:", skills[s][c.NUM_PARAM], skills[s][c.TYPE_PARAM],
-                "=> Out:", skills[s][c.NUM_RET], skills[s][c.TYPE_RET])
-        print("\t Exit")
+            # print skill name
+            print("\t{} - {}".format(s.capitalize(), skills[s][c.DESC]))
+
+            # print skill inputs/outputs
+            if skills[s][c.NUM_PARAM] != 0:
+                print("\t\tIn: {} {}".format(skills[s][c.NUM_PARAM], skills[s][c.TYPE_PARAM]), end="")
+            else:
+                print("\t\tIn: None", end="")
+
+            print(" => ", end="")
+
+            if skills[s][c.NUM_RET] != 0:
+                print("Out: {} {}".format(skills[s][c.NUM_RET], skills[s][c.TYPE_RET]))
+            else:
+                print("Out: None")
+
+        # manually add exit skill
+        print("\tExit")
